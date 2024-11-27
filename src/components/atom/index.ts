94f0d4a3e78bdfcc683aa022f404b9ab77e9cae6
ShanManage <lakshan2015dk@gmail.com>
2024-11-27 21:37:28 +0530

    feat: init input component

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -5,3 +5,4 @@
 // All essential and reusable components should be defined here to serve as the building blocks for more complex components across the application. Each component within this folder is a fundamental unit with minimal dependencies.
 
 export { default as EcButton } from './e-com-button/EcButton'
+export { default as EcInput } from './e-com-input/EcInput'
