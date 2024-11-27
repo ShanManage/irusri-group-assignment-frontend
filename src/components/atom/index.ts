479b1c916a6267ba85d55ce0a5c5eac29776d644
ShanManage <lakshan2015dk@gmail.com>
2024-11-27 21:02:21 +0530

    feat: init button component

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -3,3 +3,5 @@
 // - Input Fields
 // - Icon button
 // All essential and reusable components should be defined here to serve as the building blocks for more complex components across the application. Each component within this folder is a fundamental unit with minimal dependencies.
+
+export { default as EcButton } from './e-com-button/EcButton'
