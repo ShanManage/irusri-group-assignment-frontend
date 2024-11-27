c0bf059d63cb86dfb753aa6dd49cc87d9f6f0ed4
ShanManage <lakshan2015dk@gmail.com>
2024-11-27 22:34:00 +0530

    feat: init EcProductCard component

--- src/components/composite/index.ts
+++ src/components/composite/index.ts
@@ -3,3 +3,5 @@
 // - footer
 // - product card 
 // This folder contains higher-level components composed of multiple atomic or other composite components.
+
+export { default as EcProductCard } from './e-com-product-card/EcProductCard'
